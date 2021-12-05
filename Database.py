83cbee65ad9e2fb16c2983c275f80f0bcfd611d7
Henning Sander <janhenningsander@t-online.de>
2021-12-05 14:20:19 +0100

    - verschiedene Auflösungen für cropped_images zur Datenbank hinzugefügt - Progressbars hinzugefügt - Dynamische Auswahl der Auflösungen hinzugefügt

--- Database.py
+++ Database.py
@@ -3,6 +3,7 @@ import sqlite3
 
 import cv2
 import numpy as np
+from PyQt5.QtWidgets import QApplication
 
 import mosaic
 
@@ -18,7 +19,10 @@ class Database:
         cursor.execute('''CREATE TABLE IF NOT EXISTS image (
                             image_id INTEGER PRIMARY KEY AUTOINCREMENT,
                             img_original TEXT,
-                            img_cropped TEXT,
+                            img_cropped_32 TEXT,
+                            img_cropped_64 TEXT,
+                            img_cropped_128 TEXT,
+                            img_cropped_256 TEXT,
                             color_r REAL,
                             color_g REAL,
                             color_b REAL);''')
@@ -27,26 +31,52 @@ class Database:
     def __del__(self):
         self.connection.close()
 
-    def saveImg(self, img_original, img_cropped, color, cursor):
-        cursor.execute('''INSERT INTO image (img_original, img_cropped, color_r, color_g, color_b)
-                                VALUES (?,?,?,?,?)''', (self.encode(img_original), self.encode(img_cropped), color[0], color[1], color[2]))
+    def saveImg(self, img_original, img_cropped_32, img_cropped_64, img_cropped_128, img_cropped_256, color, cursor):
+        cursor.execute('''INSERT INTO image (img_original, img_cropped_32, img_cropped_64, img_cropped_128, img_cropped_256, color_r, color_g, color_b)
+                                VALUES (?,?,?,?,?,?,?,?)''',
+                       (self.encode(img_original), self.encode(img_cropped_32), self.encode(img_cropped_64),
+                        self.encode(img_cropped_128), self.encode(img_cropped_256), color[0], color[1], color[2]))
 
-    def importAllImages(self, folderPath):
+    def importAllImages(self, folderPath, progressBar):
         cursor = self.newCursor()
-        for filename in os.listdir(folderPath):
+        listdir = os.listdir(folderPath)
+        progressBar.setMaximum(len(listdir))
+        img_counter = 0
+        progressBar.setValue(img_counter)
+        progressBar.setVisible(True)
+        QApplication.processEvents()
+
+        for filename in listdir:
             img = mosaic.rgbImport(os.path.join(folderPath, filename))
             if img is not None:
-                self.saveImg(img, mosaic.quadsize(img), mosaic.getColorValue(img), cursor)
+                self.saveImg(img,
+                             mosaic.quadsize(img, 32),
+                             mosaic.quadsize(img, 64),
+                             mosaic.quadsize(img, 128),
+                             mosaic.quadsize(img, 256),
+                             mosaic.getColorValue(img),
+                             cursor)
+            img_counter += 1
+            progressBar.setValue(img_counter)
+            QApplication.processEvents()
         cursor.connection.commit()
 
     def getAllCroppedImages(self):
         cursor = self.newCursor()
-        cursor.execute('''SELECT img_cropped FROM image''')
+        cursor.execute('''SELECT img_cropped_32 FROM image''')
         return cursor
 
-    def getCroppedImagesWithIDByID(self, ids):
+    def getCroppedImagesWithIDByID(self, ids, elementSize):
         cursor = self.newCursor()
-        sql = "SELECT image_id, img_cropped FROM image WHERE image_id IN ({seq})".format(seq=','.join(['?'] * len(ids)))
+        if elementSize == "32":
+            sql = "SELECT image_id, img_cropped_32 FROM image WHERE image_id IN ({seq})".format(seq=','.join(['?'] * len(ids)))
+        elif elementSize == 64:
+            sql = "SELECT image_id, img_cropped_64 FROM image WHERE image_id IN ({seq})".format(seq=','.join(['?'] * len(ids)))
+        elif elementSize == 128:
+            sql = "SELECT image_id, img_cropped_128 FROM image WHERE image_id IN ({seq})".format(seq=','.join(['?'] * len(ids)))
+        else:
+            sql = "SELECT image_id, img_cropped_256 FROM image WHERE image_id IN ({seq})".format(seq=','.join(['?'] * len(ids)))
+
         cursor.execute(sql, ids)
         return cursor
 
@@ -71,3 +101,8 @@ class Database:
     def decode(self, img_string):
         nparr = np.fromstring(img_string, np.uint8)
         return cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    def deleteLibrary(self):
+        cursor = self.newCursor()
+        cursor.execute('''DELETE FROM image''')
+        self.connection.commit()
--- mosaic.py
+++ mosaic.py
@@ -1,12 +1,10 @@
-import cv2
-import matplotlib.pyplot as plt
 import math
 import os
-import numpy as np
-import matplotlib
 
-matplotlib.rcParams['figure.figsize'] = [12, 12]
-matplotlib.rcParams['figure.dpi'] = 72
+import cv2
+import numpy as np
+from PyQt5.QtWidgets import QApplication
+
 
 # Bild importieren mit RGB Konvertierung
 def rgbImport(path):
@@ -16,7 +14,7 @@ def rgbImport(path):
 
 
 # Beliebiges Bild quadratisch zuschneiden
-def quadsize(img):
+def quadsize(img, size):
     height, width, channels = img.shape
 
     if width > height:
@@ -28,7 +26,7 @@ def quadsize(img):
         hEnd = hStart + width
         cropped_image = img[hStart:hEnd, :]
 
-    return cv2.resize(cropped_image, (256, 256))
+    return cv2.resize(cropped_image, (size, size))
 
 
 # Komprimierungsansatz mittels dynamischer Verpixelung
@@ -39,8 +37,8 @@ def destroyImgDyn(img):
 
 
 # Komprimierungsansatz auf feste Größe (TODO: auch Hochkanntbilder sollen unterstützt werden)
-def destroyImgFix(img):
-    img = cv2.resize(img, (128, 72))
+def destroyImgFix(img, width, height):
+    img = cv2.resize(img, (width, height))
     return img
 
 
@@ -58,7 +56,7 @@ def getColorValue(img):
 # Euklidische Distanz berechen (Farbunterschied)
 def getColorDifference(color1, color2):
     return math.sqrt((int(color1[0]) - int(color2[0])) ** 2 + (int(color1[1]) - int(color2[1])) ** 2 + (
-                int(color1[2]) - int(color2[2])) ** 2)
+            int(color1[2]) - int(color2[2])) ** 2)
 
 
 #  Funktion, die Bilder importiert
@@ -89,11 +87,13 @@ def getAllColorValues(allImages):
 
 
 #  Erstellt das MosAIc
-def createMosaic(originImg, allColorValuesWithIDs, db):
+def createMosaic(originImg, allColorValuesWithIDs, elementSize, db, progressBar):
     ids = allColorValuesWithIDs[:, 0]
     colorValues = allColorValuesWithIDs[:, 1:]
-    originImg = destroyImgFix(originImg)
     height, width, channels = originImg.shape
+
+    progressBar.setMaximum(height * width * 2)
+    progressBarValue = 0
 
     id_matrix = []
     for y in range(height):
@@ -108,12 +108,15 @@ def createMosaic(originImg, allColorValuesWithIDs, db):
                     minDifID = ids[i]
 
             id_matrix_row.append(minDifID)
+            progressBarValue += 1
+            progressBar.setValue(progressBarValue)
+            QApplication.processEvents()
         if y == 0:
             id_matrix = id_matrix_row
         else:
             id_matrix = np.vstack([id_matrix, id_matrix_row])
 
-    croppedImagesWithIDs = np.array(db.getCroppedImagesWithIDByID(np.unique(id_matrix)).fetchall())
+    croppedImagesWithIDs = np.array(db.getCroppedImagesWithIDByID(np.unique(id_matrix), elementSize).fetchall())
 
     mosaic_img = []
     for id_matrix_row in id_matrix:
@@ -121,6 +124,9 @@ def createMosaic(originImg, allColorValuesWithIDs, db):
         for id in id_matrix_row:
             img_to_append = db.decode(croppedImagesWithIDs[croppedImagesWithIDs[:, 0].astype(int) == id, 1])
             mosaic_row.append(img_to_append)
+            progressBarValue += 1
+            progressBar.setValue(progressBarValue)
+            QApplication.processEvents()
         mosaic_img.append(np.concatenate(mosaic_row, axis=1))
 
     return cv2.cvtColor(np.concatenate(mosaic_img, axis=0), cv2.COLOR_BGR2RGB)
